0a34f605f57de3f07842c91dbbf40b589c5edb58
annabelle <anita.kntu@gmail.com>
2021-07-06 17:22:24 +0200

    initial commit

---
+++ Wine Quality Prediction.R
@@ -0,0 +1,6 @@
+#*****************************************************************************#
+#*Please read the R notebook file and following report to know my work        #
+#* Created By Anahita Esfandiaryfard                                          #
+#* Master if Data science                                                     #
+#* University of Catania                                                      #
+#* ***************************************************************************#
